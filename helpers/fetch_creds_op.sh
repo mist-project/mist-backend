04949e4878afda233180cc0f19ad8e77621fd339
Omar Cruz Pantoja <opantoja3@gatech.edu>
2025-08-03 23:41:52 -0400

    try out a difference approach for the deploy

--- helpers/fetch_creds_op.sh
+++ helpers/fetch_creds_op.sh
@@ -2,22 +2,35 @@
 echo "https://downloads.1password.com/linux/alpinelinux/stable/" >> /etc/apk/repositories
 wget https://downloads.1password.com/linux/keys/alpinelinux/support@1password.com-61ddfc31.rsa.pub \
      -P /etc/apk/keys
-apk update && apk add 1password-cli
+apk update && apk add 1password-cli && apk add coreutils
 
 # verify install
 op --version
 
 # — fetch credentials from 1Password
 TENANT_USERNAME=$(op item get $TENANT_ACCOUNT_ID --vault "$OP_VAULT" --fields TENANT_USERNAME --reveal)
-TENANT_PRIVATE_SSH_KEY=$(op item get $TENANT_ACCOUNT_ID --vault "$OP_VAULT" --fields TENANT_PRIVATE_SSH_KEY)
+TENANT_PRIVATE_SSH_KEY=$(op item get $TENANT_ACCOUNT_ID --vault "$OP_VAULT" --fields TENANT_PRIVATE_SSH_KEY | tr -d '"')
 TENANT_ADDRESS=$(op item get $TENANT_ACCOUNT_ID --vault "$OP_VAULT" --fields TENANT_ADDRESS)
 
-# append to tmpenvs and load it
-KEY_FILE=$(mktemp)
-touch .tmpenvs
-echo "TENANT_USERNAME=$TENANT_USERNAME" >> .tmpenvs
-echo "TENANT_PRIVATE_SSH_KEY_FILE=$KEY_FILE" >> .tmpenvs
-echo "TENANT_ADDRESS=$TENANT_ADDRESS" >> .tmpenvs
 
-chmod 600 "$KEY_FILE"
-echo "$TENANT_PRIVATE_SSH_KEY" > "$KEY_FILE"
+# Define file paths
+TENANT_USERNAME_FILE="tenant_username.yml"
+KEY_FILE="tenant_private_ssh_key.yml"
+
+# TENANT_ADDRESS_FILE="/tmp/tenant_address.json"
+
+# Create files if they do not exist
+touch $TENANT_USERNAME_FILE
+touch $KEY_FILE
+# chmod 600 "$KEY_FILE"
+# touch $TENANT_ADDRESS_FILE
+
+# Add private keys to their corresponding files
+# echo "ansible_user: $TENANT_USERNAME" > "$TENANT_USERNAME_FILE"
+echo "ansible_user: $TENANT_USERNAME" > "$TENANT_USERNAME_FILE"
+
+echo "ansible_ssh_private_key_file: |" > "$KEY_FILE"
+echo -e "$TENANT_PRIVATE_SSH_KEY" >> "$KEY_FILE"
+truncate -s -1 $KEY_FILE
+# echo "$TENANT_PRIVATE_SSH_KEY" > "$KEY_FILE"
+
